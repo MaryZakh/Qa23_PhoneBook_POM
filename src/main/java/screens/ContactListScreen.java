0ed00ac05085edc6a382e9cc359b9695a1971ef8
Maria Z <z0559882272@gmail.com>
2024-02-19 21:01:00 +0200

    add new contact success

--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -2,10 +2,13 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
-public class ContactListScreen extends BaseScreen{
+import java.util.List;
+
+public class ContactListScreen extends BaseScreen {
     public ContactListScreen(AppiumDriver<AndroidElement> driver) {
         super(driver);
     }
@@ -19,18 +22,46 @@ public class ContactListScreen extends BaseScreen{
     @FindBy(xpath = "//*[@text = 'Logout']")
     AndroidElement logoutBtn;
 
-    public boolean isActivityTitleDisplayed(String text){
-       // return activityTextView.getText().contains("Contact list");
-        return isShouldHave(activityTextView,text,8);
+    @FindBy(xpath = "//*[@content-desc='add']")
+    AndroidElement plusBtn;
+
+    @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/rowName']")
+    List<AndroidElement> contactNameList;
+
+    public boolean isActivityTitleDisplayed(String text) {
+        // return activityTextView.getText().contains("Contact list");
+        return isShouldHave(activityTextView, text, 8);
     }
 
-    public AuthenticationScreen logout(){
+    public AuthenticationScreen logout() {
         menuOptions.click();
         logoutBtn.click();
         return new AuthenticationScreen(driver);
     }
-    public ContactListScreen isAccountOpened(){
+
+    public ContactListScreen isAccountOpened() {
         Assert.assertTrue(isActivityTitleDisplayed("Contact list"));
+        return this;
+    }
+
+    public AddNewContactScreen openContactForm() {
+        should(activityTextView,5);
+        plusBtn.click();
+        return new AddNewContactScreen(driver);
+    }
+
+    public ContactListScreen isContactAddedByName(String name, String lastName) {
+        // List<AndroidElement>list = driver.findElements(By.xpath(""));
+        isShouldHave(activityTextView, "Contact list", 5);
+        boolean isPresent = false;
+
+        for (AndroidElement el : contactNameList) {
+            if (el.getText().equals(name + " " + lastName)) {
+                isPresent = true;
+                break;
+            }
+        }
+        Assert.assertTrue(isPresent);
         return this;
     }
 }
