2255b3e6c17f83219281531df9240eb6cb100958
Maria Z <z0559882272@gmail.com>
2024-02-22 09:37:30 +0200

    newContactSuccessRequiredFields

--- src/test/java/tests/AddNewContactsTests.java
+++ src/test/java/tests/AddNewContactsTests.java
@@ -41,7 +41,19 @@ public class AddNewContactsTests extends AppiumConfig {
 
     @Test
     public void createNewContactSuccessReq() {
-
+        int i = new Random().nextInt(1000) + 1000;
+        Contact contact = Contact.builder()
+                .name("Simon")
+                .lastName("Wow" + i)
+                .email("wow" + i + "@gmail.com")
+                .phone("65569845" + i)
+                .address("NY")
+                .build();
+        new ContactListScreen(driver)
+                .openContactForm()
+                .fillContactForm(contact)
+                .submitContactForm()
+                .isContactAddedByName(contact.getName(), contact.getLastName());
     }
 
 
